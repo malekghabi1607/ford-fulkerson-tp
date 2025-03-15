0923764d03324de9f24c44b1c54ef45729fde55a
Ghabi malek <malekghabi129@gmail.com>
2025-03-15 08:06:01 +0100

    Mise à jour du projet

--- main.cpp
+++ main.cpp
@@ -4,24 +4,36 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    cout << "Test de l'algorithme Ford-Fulkerson" << endl;
 
-    cout << "Écrire dans le main vos propres tests" << endl;
-    
-    int matrice1[6][6] = {{0,6,6,0,0,0},{6,0,0,5,0,0},
-                          {6,0,0,4,3,0},{0,5,4,0,0,7},
-                          {0,0,3,0,0,4},{0,0,0,7,4,0}
-     };
-                
-    int matrice2[6][6] = {{0,0,0,0,0,0},{0,0,0,0,0,0},
-                          {0,0,0,0,0,0},{0,0,0,0,0,0},
-                          {0,0,0,0,0,0},{0,0,0,0,0,0}
-     };
-                         
-    int* c[] = {&matrice1[0][0], &matrice1[1][0], &matrice1[2][0], &matrice1[3][0], &matrice1[4][0], &matrice1[5][0]};
-    int* f[] = {&matrice2[0][0], &matrice2[1][0], &matrice2[2][0], &matrice2[3][0], &matrice2[4][0], &matrice2[5][0]};
+    // Définition de la matrice des capacités (graphe donné dans l'énoncé)
+    int matrice1[6][6] = {
+        {0, 6, 6, 0, 0, 0},
+        {6, 0, 0, 5, 0, 0},
+        {6, 0, 0, 4, 3, 0},
+        {0, 5, 4, 0, 0, 7},
+        {0, 0, 3, 0, 0, 4},
+        {0, 0, 0, 7, 4, 0}};
 
-    fordfulkerson(c,f,6,0,5);
-    
-    affichage(c,f,6);      
+    // Matrice des flots initialement à 0
+    int matrice2[6][6] = {0};
+
+    // Pointeurs vers les matrices
+    int *c[6], *f[6];
+    for (int i = 0; i < 6; i++)
+    {
+        c[i] = matrice1[i];
+        f[i] = matrice2[i];
+    }
+
+    // Calcul du flot maximum de la source (0) au puits (5)
+    int maxFlow = fordfulkerson(c, f, 6, 0, 5);
+
+    // Affichage des résultats
+    cout << "Flot maximum trouvé : " << maxFlow << endl;
+    affichage(c, f, 6);
+
+    return 0;
 }

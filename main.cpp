adc34126afd1e114080d54d056f3a0e5363417b0
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-21 14:36:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-
 #include <iostream>
 #include "utilities.h"
 
 using namespace std;
 
-int main()
-{
+int main() {
     cout << "Test de l'algorithme Ford-Fulkerson" << endl;
-
+    
     // Définition de la matrice des capacités (graphe donné dans l'énoncé)
     int matrice1[6][6] = {
         {0, 6, 6, 0, 0, 0},
@@ -15,25 +13,25 @@ int main()
         {6, 0, 0, 4, 3, 0},
         {0, 5, 4, 0, 0, 7},
         {0, 0, 3, 0, 0, 4},
-        {0, 0, 0, 7, 4, 0}};
-
+        {0, 0, 0, 7, 4, 0}
+    };
+    
     // Matrice des flots initialement à 0
     int matrice2[6][6] = {0};
 
     // Pointeurs vers les matrices
-    int *c[6], *f[6];
-    for (int i = 0; i < 6; i++)
-    {
+    int* c[6], * f[6];
+    for (int i = 0; i < 6; i++) {
         c[i] = matrice1[i];
         f[i] = matrice2[i];
     }
 
     // Calcul du flot maximum de la source (0) au puits (5)
     int maxFlow = fordfulkerson(c, f, 6, 0, 5);
-
+    
     // Affichage des résultats
     cout << "Flot maximum trouvé : " << maxFlow << endl;
-    affichage(c, f, 6);
+    affichage(c, f, 6);  
 
     return 0;
 }
